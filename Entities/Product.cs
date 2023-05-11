9c1251c64c18f696fea772c83c973c7a07e309bc
mayaahmedabdelsameaa <mayarahmed8624@gmail.com>
2023-05-12 01:41:58 +0300

    project

---
+++ Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Entities
+{
+	public class Product
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public int Code { get; set; }
+		public List<string> UnitsOfMeasure { get; set; }
+		public virtual Store Store { get; set; }
+		public virtual ImportPermit ImportPermit { get; set; }
+        public virtual ICollection<ImportPermitDetails> ImportPermitDetails { get; set; }
+        public virtual ICollection<ExchangePermitDetails> ExchangePermitDetails { get; set; }
+
+
+    }
+}
